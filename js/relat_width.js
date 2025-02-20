f3e974736e58e6238c7520f5f2432acb6cfbaa9d
Grigoriy <foxtail25@gmail.com>
2025-02-20 10:12:29 +0300

    client-width

--- js/relat_width.js
+++ js/relat_width.js
@@ -3,9 +3,10 @@ const cardBlock = document.querySelector('.card_block');
 window.addEventListener('resize', getWidth);
 
 function getWidth(){
-    let width = window.innerWidth;
+    // let width = window.innerWidth;
+    let width = window.document.children[0].clientWidth;
     cardBlock.style.width = `${width}px`
-    console.log('width',width)
+    console.log('clientwidth', window.document.children[0].clientWidth)
 }
 
 getWidth()
